313f56e53d84d2de67497b98b6ba4819723e4dd1
irakli9909 <razora.irakli@gmail.com>
2022-05-02 10:27:46 +0400

    Done homeWork_1

--- homework_1.js
+++ homework_1.js
@@ -2,7 +2,30 @@ let row = 4;
 let col = 5;
 
 const getTable = (row, col) => {
+    let table = [];
+    let num = 1;
+    for (var i = 0; i < col; i++) {
+        if (i == 0) {
+            for (var j = 0; j < row; j++) {
+                var array = [];
+                array.push(num)
+                table.push(array);
+                num++;
+            }
+        } else if (i % 2 == 0) {
+            for (var j = 0; j < row; j++) {
+                table[j].push(num);
+                num++;
+            }
+        } else {
+            for (var j = row - 1; j >= 0; j--) {
+                table[j].push(num);
+                num++;
+            }
+        }
+    }
+    return table;
     
+   
 }
-
 console.log(getTable(row, col));
